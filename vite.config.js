f2c1a54c53f09ae2ddb01a923e71a4b686566d7b
Areliz Oyarce <arelizuwc@gmail.com>
2025-03-28 09:53:01 -0500

    feat Add pwa config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,38 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Yapita",
+        short_name: "Yapita",
+        description: "Una app gamificada para aprender finanzas diseñada para mujeres.",
+        theme_color: "#742284",
+        background_color: "#FFFFFF",
+        display: "standalone",
+        start_url: "/init",
+        icons: [
+          {
+            src: "/icon.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   esbuild: {
     loader: "jsx",
     include: /src\/.*\.js$/,
